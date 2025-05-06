483dfffc66c4042a72684e864ea9b63633464ab8
Kgomotso Kgola <kgomotso.kgola@outlook.com>
2025-05-06 13:41:13 +0200

    fixed missing images swatches

--- fronted/src/components/shop/products.tsx
+++ fronted/src/components/shop/products.tsx
@@ -10,7 +10,7 @@ const Products = () => {
     <main className="shop-main-page">
       <div className='products-cont'>
       <div className="breadcrumb">
-        <Link href="/home">Home</Link> / <Link href="/products">Shop</Link>
+        <Link href="/home">Home</Link> / <Link href="/shop">Shop</Link>
       </div>
       <h1 className="page-title">
       Our Products</h1>
